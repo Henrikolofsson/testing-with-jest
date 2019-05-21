fe9072fa6fae7c25d4cb7a0299c9d3d347123acd
Henrikolofsson <henke94_olofsson@hotmail.com>
2019-05-21 21:26:07 +0200

    Fixed

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,6 @@
-var _ = require('underscore');
 console.log("STACK");
+var _ = require('underscore');
+
 
 let stack = [];
 
@@ -15,5 +16,5 @@ exports.pop = function () {
 
 // Returnerar det översta elementet i stacken
 exports.peek = function () {
-  return stack[0]; // Det här är medvetet felaktigt
-}
+    return _.last(stack);
+  }
